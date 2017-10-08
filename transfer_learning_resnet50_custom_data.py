13819581988d71f765ad074b1084cf4a1d5999d0
Anuj Shah <anujonline645@gmail.com>
2017-10-08 21:23:37 +0530

    Add files via upload

---
+++ transfer_learning_resnet50_custom_data.py
@@ -0,0 +1,165 @@
+
+import numpy as np
+import os
+import time
+from resnet50 import ResNet50
+from keras.preprocessing import image
+from keras.layers import GlobalAveragePooling2D, Dense, Dropout,Activation,Flatten
+
+from imagenet_utils import preprocess_input
+from keras.layers import Input
+from keras.models import Model
+from keras.utils import np_utils
+from sklearn.utils import shuffle
+from sklearn.cross_validation import train_test_split
+
+img_path = 'elephant.jpg'
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
+print (x.shape)
+x = np.expand_dims(x, axis=0)
+print (x.shape)
+x = preprocess_input(x)
+print('Input image shape:', x.shape)
+
+# Loading the training data
+PATH = os.getcwd()
+# Define data path
+data_path = PATH + '/data'
+data_dir_list = os.listdir(data_path)
+
+img_data_list=[]
+
+for dataset in data_dir_list:
+	img_list=os.listdir(data_path+'/'+ dataset)
+	print ('Loaded the images of dataset-'+'{}\n'.format(dataset))
+	for img in img_list:
+		img_path = data_path + '/'+ dataset + '/'+ img 
+		img = image.load_img(img_path, target_size=(224, 224))
+		x = image.img_to_array(img)
+		x = np.expand_dims(x, axis=0)
+		x = preprocess_input(x)
+		print('Input image shape:', x.shape)
+		img_data_list.append(x)
+
+img_data = np.array(img_data_list)
+#img_data = img_data.astype('float32')
+print (img_data.shape)
+img_data=np.rollaxis(img_data,1,0)
+print (img_data.shape)
+img_data=img_data[0]
+print (img_data.shape)
+
+
+# Define the number of classes
+num_classes = 4
+num_of_samples = img_data.shape[0]
+labels = np.ones((num_of_samples,),dtype='int64')
+
+labels[0:202]=0
+labels[202:404]=1
+labels[404:606]=2
+labels[606:]=3
+
+names = ['cats','dogs','horses','humans']
+# convert class labels to on-hot encoding
+Y = np_utils.to_categorical(labels, num_classes)
+
+#Shuffle the dataset
+x,y = shuffle(img_data,Y, random_state=2)
+# Split the dataset
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=2)
+
+###########################################################################################################################
+# Custom_resnet_model_1
+#Training the classifier alone
+image_input = Input(shape=(224, 224, 3))
+
+model = ResNet50(input_tensor=image_input, include_top=True,weights='imagenet')
+model.summary()
+last_layer = model.get_layer('avg_pool').output
+x= Flatten(name='flatten')(last_layer)
+out = Dense(num_classes, activation='softmax', name='output_layer')(x)
+custom_resnet_model = Model(inputs=image_input,outputs= out)
+custom_resnet_model.summary()
+
+for layer in custom_resnet_model.layers[:-1]:
+	layer.trainable = False
+
+custom_resnet_model.layers[-1].trainable
+
+custom_resnet_model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+
+t=time.time()
+hist = custom_resnet_model.fit(X_train, y_train, batch_size=32, epochs=12, verbose=1, validation_data=(X_test, y_test))
+print('Training time: %s' % (t - time.time()))
+(loss, accuracy) = custom_resnet_model.evaluate(X_test, y_test, batch_size=10, verbose=1)
+
+print("[INFO] loss={:.4f}, accuracy: {:.4f}%".format(loss,accuracy * 100))
+
+###########################################################################################################################
+
+# Fine tune the resnet 50
+#image_input = Input(shape=(224, 224, 3))
+model = ResNet50(weights='imagenet',include_top=False)
+model.summary()
+last_layer = model.output
+# add a global spatial average pooling layer
+x = GlobalAveragePooling2D()(last_layer)
+# add fully-connected & dropout layers
+x = Dense(512, activation='relu',name='fc-1')(x)
+x = Dropout(0.5)(x)
+x = Dense(256, activation='relu',name='fc-2')(x)
+x = Dropout(0.5)(x)
+# a softmax layer for 4 classes
+out = Dense(num_classes, activation='softmax',name='output_layer')(x)
+
+# this is the model we will train
+custom_resnet_model2 = Model(inputs=model.input, outputs=out)
+
+custom_resnet_model2.summary()
+
+for layer in custom_resnet_model2.layers[:-6]:
+	layer.trainable = False
+
+custom_resnet_model2.layers[-1].trainable
+
+custom_resnet_model2.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+
+t=time.time()
+hist = custom_resnet_model2.fit(X_train, y_train, batch_size=32, epochs=12, verbose=1, validation_data=(X_test, y_test))
+print('Training time: %s' % (t - time.time()))
+(loss, accuracy) = custom_resnet_model2.evaluate(X_test, y_test, batch_size=10, verbose=1)
+
+print("[INFO] loss={:.4f}, accuracy: {:.4f}%".format(loss,accuracy * 100))
+
+############################################################################################
+import matplotlib.pyplot as plt
+# visualizing losses and accuracy
+train_loss=hist.history['loss']
+val_loss=hist.history['val_loss']
+train_acc=hist.history['acc']
+val_acc=hist.history['val_acc']
+xc=range(12)
+
+plt.figure(1,figsize=(7,5))
+plt.plot(xc,train_loss)
+plt.plot(xc,val_loss)
+plt.xlabel('num of Epochs')
+plt.ylabel('loss')
+plt.title('train_loss vs val_loss')
+plt.grid(True)
+plt.legend(['train','val'])
+#print plt.style.available # use bmh, classic,ggplot for big pictures
+plt.style.use(['classic'])
+
+plt.figure(2,figsize=(7,5))
+plt.plot(xc,train_acc)
+plt.plot(xc,val_acc)
+plt.xlabel('num of Epochs')
+plt.ylabel('accuracy')
+plt.title('train_acc vs val_acc')
+plt.grid(True)
+plt.legend(['train','val'],loc=4)
+#print plt.style.available # use bmh, classic,ggplot for big pictures
+plt.style.use(['classic'])

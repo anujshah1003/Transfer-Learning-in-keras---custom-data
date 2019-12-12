72c5d13603cc5b2d254e682dce394f7c8dd65926
Azwad Abid <38776619+AzwadAbid@users.noreply.github.com>
2019-12-12 16:19:53 +0800

    [FIX] K.image_dim_ordering() gives error

--- imagenet_utils.py
+++ imagenet_utils.py
@@ -10,7 +10,7 @@ CLASS_INDEX_PATH = 'https://s3.amazonaws.com/deep-learning-models/image-models/i
 
 def preprocess_input(x, dim_ordering='default'):
     if dim_ordering == 'default':
-        dim_ordering = K.image_dim_ordering()
+        dim_ordering = K.common.image_dim_ordering()
     assert dim_ordering in {'tf', 'th'}
 
     if dim_ordering == 'th':

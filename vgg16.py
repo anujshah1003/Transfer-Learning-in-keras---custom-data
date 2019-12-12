69b9efe665951c19ddd01b87b7370426bdefed2f
AzwadAbid <38776619+AzwadAbid@users.noreply.github.com>
2019-12-12 10:26:20 +0800

    Fixed _obtain_input_shape error
    Keras 2.2 had a minor change.
    
    Before: 
    from keras.applications.imagenet_utils import _obtain_input_shape
    
    After:
    from keras_applications.imagenet_utils import _obtain_input_shape
    
    The code is working for me after making the change.

--- vgg16.py
+++ vgg16.py
@@ -25,7 +25,7 @@ from keras.utils.data_utils import get_file
 from keras import backend as K
 from keras.applications.imagenet_utils import decode_predictions
 from keras.applications.imagenet_utils import preprocess_input
-from keras.applications.imagenet_utils import _obtain_input_shape
+from keras_applications.imagenet_utils import _obtain_input_shape
 from keras.engine.topology import get_source_inputs
 
 
